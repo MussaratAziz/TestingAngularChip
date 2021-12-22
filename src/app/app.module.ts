55ad05b107008556263bc444b2f63267fd50af33
Mussarat Aziz <mussarat.aziz@activ8intelligence.com>
2021-12-22 12:51:50 +0000

    all the changes fun

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http'
+import {MaterialExampleModule} from '../material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, MaterialExampleModule, BrowserAnimationsModule, AppRoutingModule, FormsModule, ReactiveFormsModule, MatNativeDateModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
